e6ec7d3f00946f4fd6a3537e9346147a8c8c9a74
ionlyseespots <kburas@four51.com>
2014-04-08 10:55:26 -0500

    add important rule to json formatting

--- app/js/controllers/SimpleCtrl.js
+++ app/js/controllers/SimpleCtrl.js
@@ -5,34 +5,41 @@ Simple.app.controller("SimpleCtrl", function($scope){
             "name":             "Default background",
             "class":            "navbar-default",
             "property":         "background-color",
-            "value":            "#50ACDB"
+            "value":            "#50ACDB",
+            "important":        ""
         },
         {
             "name":             "Default text",
             "parentclass":      "navbar-nav",
             "element":          "a",
             "property":         "color",
-            "value":            "#FFFFFF"
+            "value":            "#FFFFFF",
+            "important":        ""
         },
         {
             "name":             "Active background",
             "parentclass":      "navbar-nav",
-            "class":            "active",
+            "parentelement":    "li.active",
+            "element":          "a",
             "property":         "background-color",
-            "value":            "#359FD5"
+            "value":            "#359FD5",
+            "important":        " !important"
         },
         {
             "name":             "Active text",
             "parentclass":      "navbar-nav",
-            "class":            "active",
+            "parentelement":    "li.active",
+            "element":          "a",
             "property":         "color",
-            "value":            "#FFFFFF"
+            "value":            "#FFFFFF",
+            "important":        " !important"
         },
         {
             "name":             "Border",
             "parentclass":      "navbar-default",
             "property":         "border-color",
-            "value":            "#359FD5"
+            "value":            "#359FD5",
+            "important":        ""
         }
     ];
     $scope.scaffolding = [
@@ -43,19 +50,22 @@ Simple.app.controller("SimpleCtrl", function($scope){
             "parentelement":    "",
             "element":          "body",
             "property":         "background-color",
-            "value":            "#F0F0F0"
+            "value":            "#F0F0F0",
+            "important":        ""
         },
         {
             "name":             "Primary text",
             "class":            "text-primary",
             "property":         "color",
-            "value":            "#0C2A3A"
+            "value":            "#0C2A3A",
+            "important":        ""
         },
         {
             "name":             "Info text",
             "class":            "text-info",
             "property":         "color",
-            "value":            "#31708F"
+            "value":            "#31708F",
+            "important":        ""
         },
         {
             "name":             "Link text",
@@ -64,7 +74,8 @@ Simple.app.controller("SimpleCtrl", function($scope){
             "parentelement":    "body",
             "element":          "a",
             "property":         "color",
-            "value":            "#0C2A3A"
+            "value":            "#0C2A3A",
+            "important":        ""
         }
     ];
     $scope.buttons = [
@@ -72,145 +83,169 @@ Simple.app.controller("SimpleCtrl", function($scope){
             "name":         "Primary background",
             "class":        "btn-primary",
             "property":     "background-color",
-            "value":        "#0C2A3A"
+            "value":        "#0C2A3A",
+            "important":    ""
         },
         {
             "name":         "Primary text",
             "class":        "btn-primary",
             "property":     "color",
-            "value":        "#FFFFF"
+            "value":        "#FFFFF",
+            "important":    " !important"
         },
         {
             "name":         "Primary border",
             "class":        "btn-primary",
             "property":     "border-color",
-            "value":        "#0C2A3A"
+            "value":        "#0C2A3A",
+            "important":    ""
         },
         {
             "name":         "Primary hover",
             "class":        "btn-primary:hover",
             "property":     "background-color",
-            "value":        "#051118"
+            "value":        "#051118",
+            "important":    ""
         },
         {
             "name":         "Default background",
             "class":        "btn-default",
             "property":     "background-color",
-            "value":        "#FFFFFF"
+            "value":        "#FFFFFF",
+            "important":    ""
         },
         {
             "name":         "Default text",
             "class":        "btn-default",
-            "property":     "background-color",
-            "value":        "#333333"
+            "property":     "color",
+            "value":        "#333333",
+            "important":    " !important"
         },
         {
             "name":         "Default border",
             "class":        "btn-default",
             "property":     "border-color",
-            "value":        "#CCCCCC"
+            "value":        "#CCCCCC",
+            "important":    ""
         },
         {
             "name":         "Default hover",
-            "class":        "btn-default",
+            "class":        "btn-default:hover",
             "property":     "background-color",
-            "value":        "#EBEBEB"
+            "value":        "#EBEBEB",
+            "important":    ""
         },
         {
             "name":         "Info background",
             "class":        "btn-info",
             "property":     "background-color",
-            "value":        "#5BC0DE"
+            "value":        "#5BC0DE",
+            "important":    ""
         },
         {
             "name":         "Info text",
             "class":        "btn-info",
             "property":     "color",
-            "value":        "#FFFFFF"
+            "value":        "#FFFFFF",
+            "important":    " !important"
         },
         {
             "name":         "Info border",
             "class":        "btn-info",
             "property":     "border-color",
-            "value":        "#46B8DA"
+            "value":        "#46B8DA",
+            "important":    ""
         },
         {
             "name":         "Info hover",
-            "class":        "btn-info",
+            "class":        "btn-info:hover",
             "property":     "background-color",
-            "value":        "#39B3D7"
+            "value":        "#39B3D7",
+            "important":    ""
         },
         {
             "name":         "Success background",
             "class":        "btn-success",
             "property":     "background-color",
-            "value":        "#5CB85C"
+            "value":        "#5CB85C",
+            "important":    ""
         },
         {
             "name":         "Success text",
             "class":        "btn-success",
-            "property":     "background-color",
-            "value":        "#FFFFFF"
+            "property":     "color",
+            "value":        "#FFFFFF",
+            "important":    " !important"
         },
         {
             "name":         "Success border",
             "class":        "btn-success",
             "property":     "border-color",
-            "value":        "#4CAE4C"
+            "value":        "#4CAE4C",
+            "important":    ""
         },
         {
             "name":         "Success hover",
-            "class":        "btn-success",
+            "class":        "btn-success:hover",
             "property":     "background-color",
-            "value":        "#47A447"
+            "value":        "#47A447",
+            "important":    ""
         },
         {
             "name":         "Warning background",
             "class":        "btn-warning",
             "property":     "background-color",
-            "value":        "#f0ad4e"
+            "value":        "#f0ad4e",
+            "important":    ""
         },
         {
             "name":         "Warning text",
             "class":        "btn-warning",
             "property":     "color",
-            "value":        "#FFFFFF"
+            "value":        "#FFFFFF",
+            "important":    " !important"
         },
         {
             "name":         "Warning border",
             "class":        "btn-warning",
             "property":     "border-color",
-            "value":        "#EEA236"
+            "value":        "#EEA236",
+            "important":    ""
         },
         {
             "name":         "Warning hover",
-            "class":        "btn-warning",
+            "class":        "btn-warning:hover",
             "property":     "background-color",
-            "value":        "#ED9C28"
+            "value":        "#ED9C28",
+            "important":    ""
         },
         {
             "name":         "Danger background",
             "class":        "btn-danger",
             "property":     "background-color",
-            "value":        "#D9534F"
+            "value":        "#D9534F",
+            "important":    ""
         },
         {
             "name":         "Danger text",
             "class":        "btn-danger",
             "property":     "color",
-            "value":        "#FFFFFF"
+            "value":        "#FFFFFF",
+            "important":    " !important"
         },
         {
             "name":         "Danger border",
             "class":        "btn-danger",
             "property":     "border-color",
-            "value":        "#D43F3A"
+            "value":        "#D43F3A",
+            "important":    ""
         },
         {
             "name":         "Danger hover",
-            "class":        "btn-danger",
+            "class":        "btn-danger:hover",
             "property":     "background-color",
-            "value":        "#D2322D"
+            "value":        "#D2322D",
+            "important":    ""
         }
     ];
     $scope.jumbotron = [
@@ -218,14 +253,16 @@ Simple.app.controller("SimpleCtrl", function($scope){
             "name":             "Brand background",
             "class":            "jumbotron",
             "property":         "background-color",
-            "value":            "#FFFFFF"
+            "value":            "#FFFFFF",
+            "important":        ""
         },
         {
             "name":             "Brand Text",
             "parentclass":      "jumbotron",
             "element":          "h2",
             "property":         "color",
-            "value":            "#333333"
+            "value":            "#333333",
+            "important":        ""
         }
     ];
     $scope.panels = [
@@ -235,7 +272,8 @@ Simple.app.controller("SimpleCtrl", function($scope){
             "childclass":       "panel-heading",
             "class":            "panel-title",
             "property":         "background-color",
-            "value":            "#0C2A3A"
+            "value":            "#0C2A3A",
+            "important":        ""
         },
         {
             "name":             "Primary panel text",
@@ -243,7 +281,8 @@ Simple.app.controller("SimpleCtrl", function($scope){
             "childclass":       "panel-heading",
             "class":            "panel-title",
             "property":         "color",
-            "value":            "#FFFFFF"
+            "value":            "#FFFFFF",
+            "important":        ""
         },
         {
             "name":             "Default background",
@@ -251,7 +290,8 @@ Simple.app.controller("SimpleCtrl", function($scope){
             "childclass":       "panel-heading",
             "class":            "panel-title",
             "property":         "background-color",
-            "value":            "#F5F5F5"
+            "value":            "#F5F5F5",
+            "important":        ""
         },
         {
             "name":             "Default panel text",
@@ -259,7 +299,8 @@ Simple.app.controller("SimpleCtrl", function($scope){
             "childclass":       "panel-heading",
             "class":            "panel-title",
             "property":         "color",
-            "value":            "#333333"
+            "value":            "#333333",
+            "important":        ""
         },
     ];
 	function format(d){
@@ -271,7 +312,7 @@ Simple.app.controller("SimpleCtrl", function($scope){
             var pe = i.parentelement ? i.parentelement + ' ' : '';
             var el = i.element ? i.element + ' ' : '';
             var cl = i.class ? dot + i.class + ' ' : '';
-			$scope.file += pc + cc + pe + el + cl + ' {' + i.property + ': ' + i.value + ';}\r';
+			$scope.file += pc + cc + pe + el + cl + ' {' + i.property + ': ' + i.value + i.important + ';}\r';
 		});
 	}
 	$scope.save = function() {

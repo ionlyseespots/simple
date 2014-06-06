29b7bf9c09c523754ff00417366516c60b1067d4
ionlyseespots <kburas@four51.com>
2014-06-06 14:40:22 -0500

    begin set up master for next update

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,8 @@
 var Simple = {};
 
-Simple.app = angular.module('SimpleApp',['colorpicker.module', 'FBAngular']);
+Simple.app = angular.module('SimpleApp',['ngRoute', 'colorpicker.module', 'FBAngular']).
+    config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/view1', {templateUrl: 'partials/colors.html', controller: 'colorThemeCtrl'});
+        $routeProvider.when('/view2', {templateUrl: 'partials/products.html', controller: 'productThemeCtrl'});
+        $routeProvider.otherwise({redirectTo: '/view1'});
+    }]);
